50e69e66517fe2aad145b871e96ee7b14c9d575e
rayDutra <rayanneoliveiradutra.com>
2022-09-15 21:55:19 -0300

    segundo commit

--- src/main/java/com/professorangoti/bootstrap/HomeController.java
+++ src/main/java/com/professorangoti/bootstrap/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping()
+    @GetMapping("rayanne")
     public String aula(){
         return "home";
     }
